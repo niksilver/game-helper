ef9d952797afb5279635e05234d9ad8d84fa2efd
Nik Silver <nik@niksilver.com>
2025-09-20 09:27:07 +0100

    ImageSheets: Rename private/internal variables.

--- image_sheet.py
+++ image_sheet.py
@@ -14,16 +14,16 @@ class ImageSheet:
         """
         A sheet of cards, white by default.
         """
-        self.card_width     = card_width
-        self.card_height    = card_height
-        self.width          = card_width * columns
-        self.height         = card_height * rows
-        self.columns        = columns
-        self.rows           = rows
-        self.current_column = 0
-        self.current_row    = 0
-        self.base_im = Image.new(mode  = 'RGBA',
-                                 size  = (self.width, self.height),
+        self._card_width     = card_width
+        self._card_height    = card_height
+        self._width          = card_width * columns
+        self._height         = card_height * rows
+        self._columns        = columns
+        self._rows           = rows
+        self._current_column = 0
+        self._current_row    = 0
+        self._base_im = Image.new(mode  = 'RGBA',
+                                 size  = (self._width, self._height),
                                  color = colour,
                                  )
 
@@ -33,23 +33,23 @@ class ImageSheet:
         Add the next card image onto the sheet.
         """
 
-        slot_width  = int(self.width / self.columns)
-        slot_height = int(self.height / self.rows)
-        x_pos       = self.current_column * slot_width
-        y_pos       = self.current_row * slot_height
-        scaled_im   = im.resize(size = (self.card_width, self.card_height))
-        self.base_im.paste(im   = scaled_im,
+        slot_width  = int(self._width / self._columns)
+        slot_height = int(self._height / self._rows)
+        x_pos       = self._current_column * slot_width
+        y_pos       = self._current_row * slot_height
+        scaled_im   = im.resize(size = (self._card_width, self._card_height))
+        self._base_im.paste(im   = scaled_im,
                            box  = (x_pos, y_pos),
                            mask = scaled_im,
                            )
 
-        self.current_column = (self.current_column + 1) % self.columns
-        if self.current_column == 0:
-            self.current_row = self.current_row + 1
+        self._current_column = (self._current_column + 1) % self._columns
+        if self._current_column == 0:
+            self._current_row = self._current_row + 1
 
 
     def save(self, filename):
         """
         Write the sheet to the named file.
         """
-        self.base_im.save(filename)
+        self._base_im.save(filename)
