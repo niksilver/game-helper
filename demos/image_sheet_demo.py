ac3d80d579290ae988899f9bf3a4720395eb1dd7
Nik Silver <nik@niksilver.com>
2025-10-28 17:46:22 +0000

    Add demo of image sheet.

---
+++ demos/image_sheet_demo.py
@@ -0,0 +1,28 @@
+import sys
+sys.path.append('.')     # So that we can run this from the top directory
+
+import os
+
+from image_sheet import ImageSheet
+import card_maker_demo
+
+
+gutter = 0    # No gutter for image sheets
+
+sheet = ImageSheet(card_width  = card_maker_demo.card_width,    # Units are pixels
+                   card_height = card_maker_demo.card_height,
+                   columns     = 6,
+                   rows        = 2,
+                   )
+
+sheet.add(card_maker_demo.simple('One!', gutter))
+sheet.add(card_maker_demo.simple('Two!', gutter))
+
+
+outdir  = 'demos/out'
+outfile = outdir + '/demo.png'
+if not(os.path.exists(outdir)):
+    os.mkdir('demos/out')
+sheet.save(outfile)
+print(f'Output to {outfile}')
+
