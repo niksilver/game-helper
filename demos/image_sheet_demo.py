cd040c043ad523b2bfa0f0b097c33942faa5f87b
Nik Silver <nik@niksilver.com>
2025-11-01 17:21:25 +0000

    Tidy image sheet demo.

--- demos/image_sheet_demo.py
+++ demos/image_sheet_demo.py
@@ -3,18 +3,15 @@ sys.path.append('.')     # So that we can run this from the top directory
 
 import os
 
-from image_sheet import ImageSheet
-
-import card_maker_demo
-from card_maker_demo import base_maker
-
-
 outdir  = 'demos/out'
 if not(os.path.exists(outdir)):
     os.mkdir('demos/out')
 
 
-gutter = 0    # No gutter for image sheets
+from   image_sheet     import ImageSheet
+import card_maker_demo
+from   card_maker_demo import base_maker
+
 
 sheet = ImageSheet(card_width  = base_maker.width_px,    # Units are pixels
                    card_height = base_maker.height_px,
